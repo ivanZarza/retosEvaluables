9efa10e73a78698a539d46608a9cf2c310172e12
ivanZarza <ivanzarzaestevez@gmail.com>
2025-01-14 12:50:55 +0100

    fin reto evaluable tema 6

--- modulo3/tema1/src/app/app.routes.ts
+++ modulo3/tema1/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { BooksComponent } from './pages/books/books.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent }, 
   { path: 'books', component: BooksComponent },
+  { path: 'login', component: LoginComponent },
   { path: '', pathMatch: 'full', redirectTo: 'home' }
 ];
